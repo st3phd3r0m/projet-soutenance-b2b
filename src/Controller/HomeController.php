c048bb54a4578b05002548badf4c6ac467acb364
Stéphane Derom <stephane.derom@gmail.com>
2020-07-13 14:11:13 +0200

    Masquage des coordonnées

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -22,12 +22,13 @@ class HomeController extends AbstractController
     public function index(AnnouncementsRepository $announcementsRepository, CategoriesRepository $categoriesRepository, ActivitySectorRepository $activitySectorRepository, PaginatorInterface $paginator, Request $request)
     {
 
+        // dd($this->getUser());
+
         //Récupération des données de la requete GET
         $criteria = $request->query->all();
 
         //Est-ce que le boutton de la barre de recherche a été cliqué
-        if( !empty($criteria)
-        ){
+        if (!empty($criteria)) {
             //oui
             $announcements = $paginator->paginate(
                 //Appel de la méthode de requete DQL de recherche
@@ -37,8 +38,7 @@ class HomeController extends AbstractController
                 //Nombre d'élément par page
                 5
             );
-
-        }else{
+        } else {
             //non
             $announcements = $paginator->paginate(
                 $announcementsRepository->findBy([], ['created_at' => 'DESC']),
@@ -47,7 +47,6 @@ class HomeController extends AbstractController
                 //Nombre d'élément par page
                 5
             );
-            
         }
 
         $announcementsNoPag = $announcementsRepository->findAll();
@@ -63,7 +62,7 @@ class HomeController extends AbstractController
         ]);
     }
 
-    
+
     /**
      * @Route("/show/{slug}", name="home_show")
      * @param string $slug
@@ -81,78 +80,89 @@ class HomeController extends AbstractController
 
     /**
      * @Route("/new", name="home_new", methods={"GET","POST"})
-     * 
-     * 
+     * @param Request $request
+     * @return Response
      */
     public function new(Request $request, AnnouncementsRepository $announcementsRepository): Response
     {
-        
-        $maxRef = $announcementsRepository->findMaxRef();
-        $maxRef = sprintf('%06d', $maxRef[0][1]+1 );
 
-        $announcement = new Announcements;
-        $form = $this->createForm(AnnouncementsType::class, $announcement);
-        $form->handleRequest($request);
+        if ($this->getUser()) {
 
-        if ($form->isSubmitted() && $form->isValid()) {
+            $maxRef = $announcementsRepository->findMaxRef();
+            $maxRef = sprintf('%06d', $maxRef[0][1] + 1);
 
-            //Récupération des mots-clés en tant que chaine de caractères et séparation en array avec un délimiteur ";"
-            $location = json_decode($form->get("coordinates")->getData());
-            $announcement->setCity($location->name);
-            $announcement->setPostalCode($location->cp);
-            $announcement->setDeptCode($location->deptCode);
-            $announcement->setDepartememt($location->dept);
-            $gps=['long'=>$location->long,
-                  'lat'=>$location->lat];
-            $announcement->setGpsLocation($gps);
+            $announcement = new Announcements;
+            $form = $this->createForm(AnnouncementsType::class, $announcement);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+
+                //Récupération des mots-clés en tant que chaine de caractères et séparation en array avec un délimiteur ";"
+                $location = json_decode($form->get("coordinates")->getData());
+                $announcement->setCity($location->name);
+                $announcement->setPostalCode($location->cp);
+                $announcement->setDeptCode($location->deptCode);
+                $announcement->setDepartememt($location->dept);
+                $gps = [
+                    'long' => $location->long,
+                    'lat' => $location->lat
+                ];
+                $announcement->setGpsLocation($gps);
 
 
-            //Récupération des mots-clés en tant que chaine de caractères et séparation en array avec un délimiteur ";"
-            $key_words = $form->get("key_words")->getData();
-            $key_words = explode ( ";", $key_words);
-            $key_words = array_filter($key_words);
-            $announcement->setKeyWords($key_words);
+                //Récupération des mots-clés en tant que chaine de caractères et séparation en array avec un délimiteur ";"
+                $key_words = $form->get("key_words")->getData();
+                $key_words = explode(";", $key_words);
+                $key_words = array_filter($key_words);
+                $announcement->setKeyWords($key_words);
 
-            //Récupération de la délimitation du budget
-            $price_range[0] = $form->get("price_min")->getData();
-            $price_range[1] = $form->get("price_max")->getData();
-            $announcement->setPriceRange($price_range);
-            
-            $announcement->setUser($this->getUser());
-            $announcement->setRef($maxRef);
-            $announcement->setCreatedAt(new \DateTime('now'));
-            
-            $announcement->setUnlockCount(0);
+                //Récupération de la délimitation du budget
+                $price_range[0] = $form->get("price_min")->getData();
+                $price_range[1] = $form->get("price_max")->getData();
+                $announcement->setPriceRange($price_range);
 
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($announcement);
-            $entityManager->flush();
+                $announcement->setUser($this->getUser());
+                $announcement->setRef($maxRef);
+                $announcement->setCreatedAt(new \DateTime('now'));
+
+                $announcement->setUnlockCount(0);
+
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($announcement);
+                $entityManager->flush();
+
+                //Envoi d'un message de succès
+                $this->addFlash('success', 'Votre annonce a bien été enregistrée.');
+
+                return $this->redirectToRoute('home');
+            }
+
+            return $this->render('home/new.html.twig', [
+                'form' => $form->createView(),
+            ]);
+        } else {
 
             //Envoi d'un message de succès
-            $this->addFlash('success', 'Votre annonce a bien été enregistrée.');
+            $this->addFlash('pleaseConnect', 'Veuillez vous connecter pour publier une annonce.');
 
-            return $this->redirectToRoute('home');
-
+            return $this->redirectToRoute('app_login');
         }
-
-        return $this->render('home/new.html.twig', [
-            'form' => $form->createView(),
-        ]);
     }
 
 
 
-     /**
-      * @Route("/search", name="search")
-      */
-    public function search(PaginatorInterface $paginator, Request $request){
-      $announcements = $paginator->paginate(
-         $this->getDoctrine()->getRepository(Announcements::class)->search($request->query->get('search')),
-         $request->query->getInt('page, 1'),
-         10
-      );
-      return $this->render('home/index.html.twig', [
-          'announcements' => $announcements
-      ]);
+    /**
+     * @Route("/search", name="search")
+     */
+    public function search(PaginatorInterface $paginator, Request $request)
+    {
+        $announcements = $paginator->paginate(
+            $this->getDoctrine()->getRepository(Announcements::class)->search($request->query->get('search')),
+            $request->query->getInt('page, 1'),
+            10
+        );
+        return $this->render('home/index.html.twig', [
+            'announcements' => $announcements
+        ]);
     }
 }
