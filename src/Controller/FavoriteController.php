b57d823fcc1147a5e7b60dd99f47cedec26a0b14
Stéphane Derom <stephane.derom@gmail.com>
2020-07-16 21:49:23 +0200

    page favoris ok

--- src/Controller/FavoriteController.php
+++ src/Controller/FavoriteController.php
@@ -1,8 +1,13 @@
 <?php
 
 namespace App\Controller;
+
 use App\Entity\Announcements;
+use App\Entity\Users;
+use App\Repository\AnnouncementsRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -11,13 +16,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class FavoriteController extends AbstractController
 {
     /**
-     * @Route("/favorite", name="myfavorite")
+     * @Route("/favorite/{id}", name="myfavorite")
      */
-    public function index()
+    public function index(Users $user, PaginatorInterface $paginator, Request $request)
     {
-        return $this->render('favorite/index.html.twig', [
-            'favoris' => 'FavoriteController',
-            
+        //Affichage, dans l'espace utilisateur, des annonces qu'il a publiées
+        $favoris = $paginator->paginate(
+            $user->getFavoris(),
+            //Le numero de la page, si aucun numero, on force la page 1
+            $request->query->getInt('page', 1),
+            //Nombre d'élément par page
+            5
+        );
+
+        return $this->render('profile/listAnnouncements.html.twig', [
+            'announcements' => $favoris,
+            'favoris'=> true
         ]);
     }
 
@@ -32,14 +46,13 @@ class FavoriteController extends AbstractController
         $em->persist($announcement);
         $em->flush();
 
-        // $this->addFlash(
-        // '	type',
-        // '	flashMessage'
-        // );
+        //Envoi d'un message de succès
+        $this->addFlash('success', 'L\'annonce a bien été enregistrée dans vos favoris.');
+
         return $this->redirectToRoute('home');
     }
 
-     /**
+    /**
      * @Route("/favorite/retrait/{id}", name="retrait_favoris")
      */
     public function retraitFavoris(Announcements $announcement)
@@ -50,7 +63,9 @@ class FavoriteController extends AbstractController
         $em->persist($announcement);
         $em->flush();
 
+        //Envoi d'un message de succès
+        $this->addFlash('success', 'L\'annonce a bien été retirée de vos favoris.');
+
         return $this->redirectToRoute('home');
     }
-
 }
